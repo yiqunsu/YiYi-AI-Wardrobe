d92bd2f8e657613aa96908326d777959e7ae2a2a
yiqunsu <yiqunsu02@gmail.com>
2026-03-10 15:33:58 -0700

    fix: suppress hydration warning, move devtools to devDependencies

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -77,6 +77,7 @@ export default function RootLayout({
       </head>
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
+        suppressHydrationWarning
       >
         <Providers>
           <AuthProvider>
